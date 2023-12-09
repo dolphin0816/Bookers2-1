3a9a93d9cc750349a9811dba6465ea5b7555ea0f
EC2 Default User <ec2-user@ip-172-31-19-71.ap-northeast-1.compute.internal>
2023-12-09 17:09:39 +0000

    [bookers2]userpage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book.id)
   end
   
   def index
@@ -17,6 +17,13 @@ class BooksController < ApplicationController
   end
 
   def show
+     @book = Book.find(params[:id]) 
+  end
+  
+  def destroy
+    book = Book.find(params[:id]) 
+    book.destroy
+    redirect_to '/books'
   end
   
   # 投稿データのストロングパラメータ
